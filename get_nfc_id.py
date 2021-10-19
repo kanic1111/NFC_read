3cf367993603875349cdceb54607dc1f06f2cd14
kanic1111 <daniel890723@gmail.com>
2021-10-19 14:18:40 +0800

    add read NFC id

--- get_nfc_id.py
+++ get_nfc_id.py
@@ -1,15 +1,18 @@
 import nfc
-import time
+import time,os
 import paho.mqtt.client as mqtt
 clf = nfc.ContactlessFrontend('usb:001:005')
 from binascii import hexlify
 from nfc.clf import RemoteTarget
+from dotenv import load_dotenv
+
+load_dotenv()
 client = mqtt.Client()
 while(1):
     try:
         target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
         if(target is not None):
-            client.connect("10.0.0.132",1883)
+            client.connect(os.getenv('SERVER-IP'),1883)
             nfc_id = hexlify(target.sdd_res).decode()
             print(nfc_id)
             client.publish("nfc_id", nfc_id)
